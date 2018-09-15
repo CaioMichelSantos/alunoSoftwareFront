4195321604e542e44e6a8b3b323fd766879180c8
Danrley Morais <danrley.morais@linkapi.com.br>
2018-09-14 22:17:35 -0300

    storage

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -4,16 +4,27 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class StorageService {
-
     constructor() { }
 
     set(key, value) {
-        const data = value;
-        localStorage.setItem(key, value);
+        localStorage.setItem(key, this.convertJSONToStr(value));
     }
 
     get(key) {
         const data = localStorage.getItem(key);
-        return data;
+        return this.convertStrToJSON(data);
+    }
+
+    private convertJSONToStr(obj) {
+        return typeof obj === 'object' ? JSON.stringify(obj) : obj;
+    };
+
+    private convertStrToJSON(obj) {
+        try {
+            const converted = JSON.parse(obj);
+            return converted;
+        } catch (err) {
+            return obj;
+        }
     }
 }
