1b222b53a1c5e02e50b9dfa2cd0cddb19f1f5d4f
Danrley Morais <danrley.morais@linkapi.com.br>
2018-11-20 20:27:09 -0200

    evaluation

--- src/app/components/evaluations/evaluations.component.ts
+++ src/app/components/evaluations/evaluations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EvaluationService } from 'src/app/services/evaluation.service';
 
 @Component({
   selector: 'app-evaluations',
@@ -7,21 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EvaluationsComponent implements OnInit {
 
+  months: any = [
+    { key: 'Janeiro', value: '1' },
+    { key: 'Fevereiro', value: '2' },
+    { key: 'Março', value: '3' },
+    { key: 'Abril', value: '4' },
+    { key: 'Maio', value: '5' },
+    { key: 'Junho', value: '6' },
+    { key: 'Julho', value: '7' },
+    { key: 'Agosto', value: '8' },
+    { key: 'Setembro', value: '9' },
+    { key: 'Outubro', value: '10' },
+    { key: 'Novembro', value: '11' },
+    { key: 'Dezembro', value: '12' },
+  ];
 
-  constructor() {
+  month: any;
+  year: any;
+
+  evaluations: any = [];
+  evaluationSelected: any = {};
+  constructor(private evaluationService: EvaluationService) {
   }
+
   visibleEvaluation
   visibleDetailEvaluation
 
   ngOnInit() {
+    this.visibleEvaluation = true;
+    this.visibleDetailEvaluation = false;
+    this.loadEvaluationByUser();
+  }
 
+  async loadEvaluationByUser() {
+    try {
+      this.evaluations = (await this.evaluationService.get())['evaluations'];
+    } catch (err) {
+
+    }
   }
 
   openVisibleEvaluation() {
     this.visibleEvaluation = true
     this.visibleDetailEvaluation = false
   }
-  openVisibleDetailEvaluation() {
+
+  openVisibleDetailEvaluation(evaluation) {
+    this.evaluationSelected = evaluation;
     this.visibleDetailEvaluation = true
     this.visibleEvaluation = false
   }
--- src/app/services/evaluation.service.ts
+++ src/app/services/evaluation.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { AuthenticationService } from './authentication.service';
+import { HttpClientService } from './http-client.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EvaluationService {
 
-  constructor() { }
+  url: any = `${environment.API_URL}/evaluation`;
+
+  constructor(private authenticationService: AuthenticationService, private httpClientService: HttpClientService) { }
+
+  get() {
+    const user = this.authenticationService.getLogged();
+    return this.httpClientService.get(`${this.url}/${user._id}`);
+  }
+
 }
