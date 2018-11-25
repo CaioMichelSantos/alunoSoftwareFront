e53081bbd039be98400cdc5fd7fa4d085aba396c
Danrley Morais <danrley.morais@linkapi.com.br>
2018-11-25 15:47:34 -0200

    Styles

--- src/app/components/progress/progress.component.ts
+++ src/app/components/progress/progress.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
+  styleUrls: ['./progress.component.scss']
 })
 export class ProgressComponent implements OnInit {
   newAulaControl: boolean
